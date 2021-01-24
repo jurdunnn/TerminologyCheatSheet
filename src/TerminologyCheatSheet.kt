ebdeb6fc5ed1547d501081073b019845cff84d9e
jurdunnn <70278289+jurdunnn@users.noreply.github.com>
2021-01-24 19:07:28 +0000

    Initial project expectations completed - TODO compare user answer with actual answer by number input

--- src/TerminologyCheatSheet.kt
+++ src/TerminologyCheatSheet.kt
@@ -1,16 +1,45 @@
 import java.io.File
+import kotlin.random.Random.Default.nextInt
 
-var questionsAndAnswersMap = mapOf<String,String>()
+var questionsAndAnswersMap = mapOf<String, String>()
+const val answerListLength = 4
 
 fun main(args: Array<String>) {
     //read the questions file
     readQuestionsFile()
-    //loop start
+
     //get random question/answer from map
+    fun randomIndex(): Int = nextInt(1, questionsAndAnswersMap.size) //get random index
+    val questionAndAnswer = questionsAndAnswersMap.entries.elementAt(randomIndex())//get question and answer grouping
+    val question = questionAndAnswer.key //get question
+    val answer = questionAndAnswer.value //get answer
+
     //get 3 more random answers for answer variety
+    val additionalAnswers = mutableListOf<String>()
+
+    additionalAnswers.add(answer) //add correct answer to list
+
+    for (i in 1..answerListLength) {
+        val possibleAdAnswer = questionsAndAnswersMap.entries.elementAt(randomIndex()).value
+        additionalAnswers.add(possibleAdAnswer)
+    }
+
+    additionalAnswers.shuffle() //shuffle list
+
     //display and wait for user choice
+    println("Question: $question")
+    for (j in 1 until additionalAnswers.size) {
+        println("$j: ${additionalAnswers[j]}")
+    }
+
+    val userAnswer = readLine() //wait for user input
+
     //compare user choice with answer
-    //loop again
+    if (userAnswer.equals(answer)) {
+        println("\n\n$answer was the correct answer! Well done!")
+    } else {
+        println("\n\n$userAnswer was not the correct answer, $answer was! Better luck next time...")
+    }
 }
 
 fun readQuestionsFile() {
