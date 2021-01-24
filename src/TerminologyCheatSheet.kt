482e511307cb93a00b108c1f0e7ff4ea4712340e
jurdunnn <70278289+jurdunnn@users.noreply.github.com>
2021-01-24 22:23:39 +0000

    Intial expectation complete

--- src/TerminologyCheatSheet.kt
+++ src/TerminologyCheatSheet.kt
@@ -8,38 +8,51 @@ fun main(args: Array<String>) {
     //read the questions file
     readQuestionsFile()
 
-    //get random question/answer from map
-    fun randomIndex(): Int = nextInt(1, questionsAndAnswersMap.size) //get random index
-    val questionAndAnswer = questionsAndAnswersMap.entries.elementAt(randomIndex())//get question and answer grouping
-    val question = questionAndAnswer.key //get question
-    val answer = questionAndAnswer.value //get answer
+    //loop questions indefinitely until break
+    while (true) {
+        //get random question/answer from map
+        fun randomIndex(): Int = nextInt(1, questionsAndAnswersMap.size) //get random index
+        val questionAndAnswer = questionsAndAnswersMap.entries.elementAt(randomIndex())//get question and answer grouping
+        val question = questionAndAnswer.key //get question
+        val answer = questionAndAnswer.value //get answer
 
-    //get 3 more random answers for answer variety
-    val additionalAnswers = mutableListOf<String>()
+        //get 3 more random answers for answer variety
+        val additionalAnswers = mutableListOf<String>()
 
-    additionalAnswers.add(answer) //add correct answer to list
+        additionalAnswers.add(answer) //add correct answer to list
 
-    for (i in 1..answerListLength) {
-        val possibleAdAnswer = questionsAndAnswersMap.entries.elementAt(randomIndex()).value
-        additionalAnswers.add(possibleAdAnswer)
+        for (i in 1..answerListLength) {
+            val possibleAdAnswer = questionsAndAnswersMap.entries.elementAt(randomIndex()).value
+            additionalAnswers.add(possibleAdAnswer)
+        }
+
+        additionalAnswers.shuffle() //shuffle list
+
+        //display and wait for user choice
+        println("\nQuestion: $question")
+        for (j in 1 until additionalAnswers.size) {
+            println("$j: ${additionalAnswers[j]}")
+        }
+
+        //for break and program exit
+        println("5: Exit Program")
+
+        val userAnswer: Int = readLine()!!.toInt() //wait for user input
+
+        //if user answer if 5, then beak from program
+        if(userAnswer == 5) {
+            break
+        }
+
+        //compare user choice with answer
+        if (additionalAnswers[userAnswer] == answer) {
+            println("\n\n$answer was the correct answer! Well done!")
+        } else {
+            println("\n\n${additionalAnswers[userAnswer]} was not the correct answer, $answer was! Better luck next time...")
+        }
     }
 
-    additionalAnswers.shuffle() //shuffle list
-
-    //display and wait for user choice
-    println("Question: $question")
-    for (j in 1 until additionalAnswers.size) {
-        println("$j: ${additionalAnswers[j]}")
-    }
-
-    val userAnswer = readLine() //wait for user input
-
-    //compare user choice with answer
-    if (userAnswer.equals(answer)) {
-        println("\n\n$answer was the correct answer! Well done!")
-    } else {
-        println("\n\n$userAnswer was not the correct answer, $answer was! Better luck next time...")
-    }
+    println("\nProgram End...")
 }
 
 fun readQuestionsFile() {
